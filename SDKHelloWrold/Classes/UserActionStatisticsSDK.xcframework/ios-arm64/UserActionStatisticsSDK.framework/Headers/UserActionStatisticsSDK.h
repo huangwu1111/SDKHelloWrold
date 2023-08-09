ae87dd4199abce0024b6d6a4054066449c9205bc
huangwu1111 <1243930473@qq.com>
2023-08-09 10:52:46 +0800

    www
    www

---
+++ SDKHelloWrold/Classes/UserActionStatisticsSDK.xcframework/ios-arm64/UserActionStatisticsSDK.framework/Headers/UserActionStatisticsSDK.h
@@ -0,0 +1,18 @@
+//
+//  UserActionStatisticsSDK.h
+//  UserActionStatisticsSDK
+//
+//  Created by whqpMac007 on 2023/8/2.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UserActionStatisticsSDK.
+FOUNDATION_EXPORT double UserActionStatisticsSDKVersionNumber;
+
+//! Project version string for UserActionStatisticsSDK.
+FOUNDATION_EXPORT const unsigned char UserActionStatisticsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserActionStatisticsSDK/PublicHeader.h>
+
+
