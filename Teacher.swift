bc4dcd86f3bace50c0b845542e8db6ffc981fb06
huangwu1111 <1243930473@qq.com>
2023-08-09 09:06:30 +0800

    增加老师

---
+++ Teacher.swift
@@ -0,0 +1,18 @@
+//
+//  Teacher.swift
+//  SDKHelloWrold
+//
+//  Created by whqpMac007 on 2023/8/9.
+//
+
+import Foundation
+
+
+public class Teacher {
+    public init() {
+        
+    }
+    public func haveStudent(num: Int) -> String {
+        "i have \(num) students"
+    }
+}
